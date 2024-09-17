f07fe7e8369e3f96d90b414089d5b36b418bd701
yuxunn <teeyuxun@gmail.com>
2024-09-17 14:53:28 +0800

    initial setup

--- index.js
+++ index.js
@@ -6,6 +6,17 @@ app.get('/', (req, res) => {
   res.send('Hello, World!');
 });
 
+app.get('/about', (req, res) => {
+  res.send('This is the About page');
+});
+
+app.get('/test', (req, res) => {
+  res.send('This is the test page');
+});
+
+
 app.listen(port, () => {
   console.log(`Server running at http://localhost:${port}`);
 });
+
+// npm run dev to get the localhost started
