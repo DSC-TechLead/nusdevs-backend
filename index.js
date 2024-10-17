c3766fe69d49bfed0aa6d7186d155cce2a10d97f
EC2 Default User <ec2-user@ip-172-31-8-234.ap-southeast-2.compute.internal>
2024-10-17 16:06:36 +0000

    setup

--- index.js
+++ index.js
@@ -1,22 +1,47 @@
 const express = require('express');
 const app = express();
-const port = 3000;
+const { Pool } = require('pg');
 
+require('dotenv').config(); 
+
+const pool = new Pool({
+    user: process.env.DB_USER,
+    host: process.env.DB_HOST,
+    database: process.env.DB_NAME,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    ssl: {
+      rejectUnauthorized: false  
+    }
+});
+
+pool.connect((err, client, release) => {
+    if (err) {
+      return console.error('Error acquiring client', err.stack);
+    }
+    console.log('Connected to PostgreSQL RDS!');
+  });
+
+app.get('/db-test', async (req, res) => {
+    console.log('Route /db-test called');
+    try {
+        const result = await pool.query('SELECT NOW()');
+        console.log('Query successful:', result);
+        res.json(result.rows);
+    } catch (err) {
+        console.error('Database error:', err);
+        res.status(500).json({ error: err.message });
+    }
+});
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
-});
-
-app.get('/about', (req, res) => {
-  res.send('This is the About page');
-});
+    res.send('Hello, World!');
+  });
 
 app.get('/test', (req, res) => {
-  res.send('This is the test page');
+    res.send('This is the test page');
 });
 
-
-app.listen(port, () => {
-  console.log(`Server running at http://localhost:${port}`);
+const port = process.env.PORT || 3000;
+app.listen(port, '0.0.0.0', () => {
+    console.log(`Server is running on port ${port}`);
 });
-
-// npm run dev to get the localhost started
