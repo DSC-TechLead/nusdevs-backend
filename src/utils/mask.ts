8766fa127343d204702d293acab9ee16c7cba5bf
gabyang <e0725795@u.nus.edu>
2024-12-12 23:53:31 +0800

    add new files for file structure

---
+++ src/utils/mask.ts
@@ -0,0 +1,14 @@
+// Template mask service for app security used for masking strings, especially for email, username, password, etc.
+const maskedAttributes = ['auth_method_token', 'user_token', 'refresh_token', 'client_secret'];
+const maskObject = (body: any): any => {
+  const maskedBody = { ...body };
+  for (const maskedAttribute of maskedAttributes) {
+    const value = maskedBody[maskedAttribute];
+    if (value && typeof value == 'string' && value.length > 5) {
+      maskedBody[maskedAttribute] = `${value.substring(0, 4)}***`;
+    }
+  }
+  return maskedBody;
+};
+
+export { maskObject };
